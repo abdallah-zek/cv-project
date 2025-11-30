864b72092bf2d82d12215049defdb91b0b9efce5
Abdallah Zekaik <abdallahzekaik1@gmail.com>
2025-11-30 16:24:08 +0100

    fixed style issues

--- src/components/GeneralInformatoin.jsx
+++ src/components/GeneralInformatoin.jsx
@@ -14,40 +14,58 @@ export default function GeneralInformation({ personalInfo, setPersonalInfo }) {
     <div className="general-info">
       <h2>General Information</h2>
 
-      <input
-        type="text"
-        placeholder="First name"
-        value={personalInfo.firstName}
-        onChange={(event) => {
-          setPersonalInfo({ ...personalInfo, firstName: event.target.value });
-        }}
-      />
+      <div className="first-name">
+        <label>First Name</label>
+        <input
+          type="text"
+          placeholder="Mohamed"
+          value={personalInfo.firstName}
+          onChange={(event) => {
+            setPersonalInfo({ ...personalInfo, firstName: event.target.value });
+          }}
+        />
+      </div>
 
-      <input
-        type="text"
-        placeholder="Last name"
-        value={personalInfo.lastName}
-        onChange={(event) => {
-          setPersonalInfo({ ...personalInfo, lastName: event.target.value });
-        }}
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        value={personalInfo.emailInput}
-        onChange={(event) => {
-          setPersonalInfo({ ...personalInfo, emailInput: event.target.value });
-        }}
-      />
+      <div className="last-name">
+        <label htmlFor="">Last Name</label>
+        <input
+          type="text"
+          placeholder="Hachi"
+          value={personalInfo.lastName}
+          onChange={(event) => {
+            setPersonalInfo({ ...personalInfo, lastName: event.target.value });
+          }}
+        />
+      </div>
 
-      <input
-        type="tel"
-        placeholder="Phone"
-        value={personalInfo.phoneInput}
-        onChange={(event) => {
-          setPersonalInfo({ ...personalInfo, phoneInput: event.target.value });
-        }}
-      />
+      <div className="email">
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="chaibon@gmail.com"
+          value={personalInfo.emailInput}
+          onChange={(event) => {
+            setPersonalInfo({
+              ...personalInfo,
+              emailInput: event.target.value,
+            });
+          }}
+        />
+      </div>
+      <div className="tel">
+        <label>Telephone</label>
+        <input
+          type="tel"
+          placeholder="05 40 80 60 70 "
+          value={personalInfo.phoneInput}
+          onChange={(event) => {
+            setPersonalInfo({
+              ...personalInfo,
+              phoneInput: event.target.value,
+            });
+          }}
+        />
+      </div>
 
       {/* <p>
         Preview: {firstName} {lastName}{" "}
